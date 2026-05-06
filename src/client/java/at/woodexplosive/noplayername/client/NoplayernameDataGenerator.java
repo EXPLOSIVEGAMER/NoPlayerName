ef6624a1aa4b4a6dac389d5503c8c8f4846f9207
WoodEXPLOSIVE <explosive.gamer2000@gmail.com>
2026-05-06 20:16:09 +0200

    Added / Changed Files

--- src/client/java/at/woodexplosive/noplayername/client/NoplayernameDataGenerator.java
+++ src/client/java/at/woodexplosive/noplayername/client/NoplayernameDataGenerator.java
@@ -7,6 +7,5 @@ public class NoplayernameDataGenerator implements DataGeneratorEntrypoint {
 
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
     }
 }
