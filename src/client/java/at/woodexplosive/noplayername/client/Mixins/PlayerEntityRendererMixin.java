a0dd8906b5e80a14785e6c081894d8ebadc3ec6b
WoodEXPLOSIVE <105720229+EXPLOSIVEGAMER@users.noreply.github.com>
2026-05-06 19:19:47 +0200

    Commit Version 1.21.11
    This is the mod for the version 1.21.11

---
+++ src/client/java/at/woodexplosive/noplayername/client/Mixins/PlayerEntityRendererMixin.java
@@ -0,0 +1,23 @@
+package at.woodexplosive.noplayername.client.Mixins;
+
+import at.woodexplosive.noplayername.client.NoPlayerNameConfig;
+import net.minecraft.client.render.command.OrderedRenderCommandQueue;
+import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import net.minecraft.client.render.entity.state.PlayerEntityRenderState;
+import net.minecraft.client.render.state.CameraRenderState;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerEntityRenderer.class)
+public abstract class PlayerEntityRendererMixin {
+
+    @Inject(method = "renderLabelIfPresent*", at = @At("HEAD"), cancellable = true)
+    private void hidePlayerNames(PlayerEntityRenderState state, MatrixStack matrices, OrderedRenderCommandQueue queue, CameraRenderState cameraRenderState, CallbackInfo ci) {
+        if (NoPlayerNameConfig.hideNametags) {
+            ci.cancel();
+        }
+    }
+}
